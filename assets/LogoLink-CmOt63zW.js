f6f359b7de2e23d66c5d93f8ebf02ecc30d47123
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2024-08-04 21:29:52 +0000

    Deploying to gh-pages from @ GoitStudentsWorks/node_fs99_fron_xssayy@095c8594a04d9a181d25793beadef51ab4c4c396 🚀

---
+++ assets/LogoLink-CmOt63zW.js
@@ -0,0 +1,2 @@
+import{j as t,x as s}from"./index-DXNsE3qg.js";const o="_title_3o5ur_1",e={title:o},r=()=>t.jsx(s,{to:"/",className:e.title,children:"Aquatrack"});export{r as L};
+//# sourceMappingURL=LogoLink-CmOt63zW.js.map
